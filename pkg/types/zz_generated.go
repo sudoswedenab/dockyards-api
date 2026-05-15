c72a4b605eb317a045e220db6fa6f80d0536c8d0
Maxim Kravchenko <maxim.kravchenko@sudosweden.com>
2026-05-15 12:59:05 +0200

    Update Cluster CRD to allow users to define per-cluster Gateway reference

--- pkg/types/zz_generated.go
+++ pkg/types/zz_generated.go
@@ -183,8 +183,28 @@ type Cluster struct {
 // and which cloud environment is being actively used, so tread carefully
 // when using these options.
 type ClusterAdvancedOptions struct {
+	// Gateway Options to override the installation-wide Gateway API configuration
+	// for this specific DockyardsCluster.
+	Gateway *ClusterGatewayOptions `json:"gateway,omitempty"`
+
 	// Kubevirt Options applied to kubevirt in case we're running in a kubevirt environment.
 	Kubevirt *ClusterKubevirtOptions `json:"kubevirt,omitempty"`
+}
+
+// ClusterGatewayOptions Options to override the installation-wide Gateway API configuration
+// for this specific DockyardsCluster.
+type ClusterGatewayOptions struct {
+	// ParentRef Gateway parentRef settings used for this specific DockyardsCluster.
+	ParentRef *ClusterGatewayParentRefOptions `json:"parentRef,omitempty"`
+}
+
+// ClusterGatewayParentRefOptions Gateway parentRef settings used for this specific DockyardsCluster.
+type ClusterGatewayParentRefOptions struct {
+	// Name Gateway name to use for this cluster.
+	Name *string `json:"name,omitempty"`
+
+	// Namespace Gateway namespace to use for this cluster.
+	Namespace *string `json:"namespace,omitempty"`
 }
 
 // ClusterKubevirtOptions Options applied to kubevirt in case we're running in a kubevirt environment.
